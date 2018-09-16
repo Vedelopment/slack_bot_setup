eea1b98c99a12d604fc04fd3f2c69b7f204ec61e
Ryan Johnson <johnson.rl@gmail.com>
2018-09-15 21:48:09 -0700

    cleanup

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+  require('dotenv').config()
 }
 ///////////////////////////////////////////////
 /////            Sever Config             /////
@@ -15,30 +15,30 @@ const port = process.env.PORT || 3000
 /////            Slack Config             /////
 ///////////////////////////////////////////////
 
-const { WebClient } = require('@slack/client');
-const { RTMClient } = require('@slack/client');
-const { IncomingWebhook } = require('@slack/client');
-const { createEventAdapter } = require('@slack/events-api');
-const { createMessageAdapter } = require('@slack/interactive-messages');
+const { WebClient } = require('@slack/client')
+const { RTMClient } = require('@slack/client')
+const { IncomingWebhook } = require('@slack/client')
+const { createEventAdapter } = require('@slack/events-api')
+const { createMessageAdapter } = require('@slack/interactive-messages')
 
 const slackToken = process.env.SLACK_TOKEN
-const slackWeb = new WebClient(slackToken);
-const slackRtm = new RTMClient(slackToken);
-const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
+const slackWeb = new WebClient(slackToken)
+const slackRtm = new RTMClient(slackToken)
+const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET)
 
 // To setup slack webhooks, uncomment this line and create a slack webhook url here: https://api.slack.com/incoming-webhooks
-// const slackWebhook = new IncomingWebhook(<Slack Webhook Url>);
+// const slackWebhook = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL)
 
 ///////////////////////////////////////////////
 /////             Sever Start             /////
 ///////////////////////////////////////////////
 
 // Starting Express Server
-app.use('/slack/events', slackEvents.expressMiddleware());
-app.use(bodyParser.urlencoded({ extended : true }));
-app.use(express.static('public'));
+app.use('/slack/events', slackEvents.expressMiddleware())
+app.use(bodyParser.urlencoded({ extended : true }))
+app.use(express.static('public'))
 http.createServer(app).listen(port, () => {
-  console.log(`server listening on port ${port}`);
+  console.log(`server listening on port ${port}`)
 });
 
 
@@ -50,11 +50,11 @@ if (process.env.START_SLACK_RTM === 'true') slackRtm.start()
 ///////////////////////////////////////////////
 
 app.get('/', function(req, res){
-  console.log(__dirname);
+  console.log(__dirname)
   res.sendFile('index.html', {
     root : __dirname
-  });
-});
+  })
+})
 
 ///////////////////////////////////////////////
 /////         Incoming Webhooks           /////
